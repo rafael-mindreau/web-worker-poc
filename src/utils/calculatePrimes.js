a43e245c22c6d506c4136b35725f404baa3aeb6d
Rafael Mindreau <rafael.mindreau@gmail.com>
2020-09-02 14:46:15 +0200

    Fix illegal const

--- src/utils/calculatePrimes.js
+++ src/utils/calculatePrimes.js
@@ -5,7 +5,7 @@ export default function calculatePrimes(iterations, multiplier) {
   const primes = [];
   for (var i = 0; i < iterations; i++) {
     const candidate = i * (multiplier * Math.random());
-    const isPrime = true;
+    let isPrime = true;
     for (var c = 2; c <= Math.sqrt(candidate); ++c) {
       if (candidate % c === 0) {
           // not prime
